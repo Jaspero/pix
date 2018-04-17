1cd40e9f3c85ad8d83579a35ba7badecaab32bb1
siclol <marko.milky@gmail.com>
2018-04-17 16:45:42 +0200

    Disable scroll on modal active

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,7 +1,7 @@
 export class Modal {
  constructor() {
    this.modal = document.querySelectorAll('[class*="modal"][id]');
-   this.modalContent = document.querySelectorAll('.modal_content');
+   this.modalContent = document.querySelectorAll('.modal-content');
    this.modalOpen = document.querySelectorAll('[data-open-modal]');
    this.modalClose = document.querySelectorAll('[data-close-modal]');
 
@@ -13,9 +13,10 @@ export class Modal {
      const modalEl = document.getElementById(el.getAttribute('data-open-modal'));
      el.addEventListener('click', () => {
        modalEl.classList.add('active');
-       const modalHeaderEl = modalEl.querySelector('.modal_header').scrollHeight;
-       const modalFooterEl = modalEl.querySelector('.modal_footer').scrollHeight;
-       const modalBodyEl = modalEl.querySelector('.modal_body');
+         document.documentElement.classList.add('modal-opened');
+       const modalHeaderEl = modalEl.querySelector('.modal-header').scrollHeight;
+       const modalFooterEl = modalEl.querySelector('.modal-footer').scrollHeight;
+       const modalBodyEl = modalEl.querySelector('.modal-body');
        modalBodyEl.style.maxHeight = ('calc(100vh - 4rem - ' + (modalHeaderEl + modalFooterEl) + 'px)');
      })
    });
@@ -29,6 +30,7 @@ export class Modal {
    this.modal.forEach(el => {
      el.addEventListener('click', () => {
        el.classList.remove('active');
+       document.documentElement.classList.remove('modal-opened');
      });
    });
 
@@ -36,6 +38,7 @@ export class Modal {
      el.addEventListener('click', () => {
        this.modal.forEach(el => {
          el.classList.remove('active');
+         document.documentElement.classList.remove('modal-opened');
        });
      });
    });
